fa1732fc3f9eca859fd1d8a6dad5fbc435b02f2d
Brett Buchanan <fullmetalsleeve@gmail.com>
2026-06-17 16:21:40 +0800

    Display fleet log data in a table format

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,29 @@
 <?php
+$logFile = "fleetlog.txt";
+$lines = file($logFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+echo "<table border='1' cellpadding='5'>";
+echo "<tr><th>Timestamp</th><th>Driver</th><th>Action</th><th>Amount</th><th>Cargo</th><th>Current Location</th></tr>";
+
+foreach ($lines as $line) {
+    list($timestamp, $driver, $action, $amount, $cargo, $slurl) = explode(" | ", $line);
+
+    echo "<tr>";
+    echo "<td>$timestamp</td>";
+    echo "<td>" . str_replace("Driver: ", "", $driver) . "</td>";
+    echo "<td>" . str_replace("Action: ", "", $action) . "</td>";
+    echo "<td>" . str_replace("Amount: ", "", $amount) . "</td>";
+    echo "<td>" . str_replace("Cargo: ", "", $cargo) . "</td>";
+
+    $slurlLink = str_replace("SLURL: ", "", $slurl);
+    echo "<td><a href='$slurlLink' target='_blank'>Teleport</a></td>";
+
+    echo "</tr>";
+}
+echo "</table>";
+?>
+<meta http-equiv="refresh" content="5">
+<?php
 // dashboard.php
 $logFile = "fleetlog.txt";
 $lines = file($logFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
